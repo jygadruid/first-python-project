41b2754689e081afc4f0349a379276d80c92d407
unknown <redpen.elijah@gmail.com>
2020-10-05 18:15:32 -0700

    conditional challenge

--- challenges/square_root.py
+++ challenges/square_root.py
@@ -2,11 +2,11 @@
 
 # Write your square_root function here:
 def square_root(num):
-    num **(1/2)
-    return num
+    return num ** (1/2)
 
 # Uncomment these function calls to test your square_root function:
 print(square_root(16))
 # should print 4
 print(square_root(100))
 #should print 10
+
